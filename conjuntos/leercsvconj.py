cc46235be56563f08335f3faac34f6d8569a0a31
jgonzalez <juapabgonzalez@utp.edu.co>
2024-06-14 09:37:50 -0500

    leer archivo y graficar conjuntos

---
+++ conjuntos/leercsvconj.py
@@ -0,0 +1,27 @@
+from matplotlib_venn import venn2
+from sympy import FiniteSet
+import matplotlib.pyplot as plt
+import csv
+
+
+def leercsv(file):
+
+    futball = set()
+    other = set()
+
+    with open(file) as f:
+        reader = csv.reader(f)
+        next(reader)
+
+        for row in reader:
+            if row[1] == '1':
+                futball.add(row[0])
+            if row[2] == '1':
+                other.add(row[0])
+    
+    venn2([futball,other])
+    plt.show()
+
+if __name__ == '__main__':
+    leercsv('sport.csv')
+            
